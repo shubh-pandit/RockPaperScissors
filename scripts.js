28adbd54dc472c73633392b8646e86f51761c380
shubh-pandit <shubhankar.pandit24@gmail.com>
2020-05-18 08:08:47 +0530

    css left

--- scripts.js
+++ scripts.js
@@ -8,8 +8,53 @@ function computerPlay(){
 
 function playRound(playerMove,computerMove){
 
-    computerMove = computerPlay();
-    playerMove = playerPlay();
 
-    if(computerMove=="rock")
+    if(computerMove=="rock"){
+        if(playerMove=="scissors")
+            return "lose";
+        else if(playerMove=="paper")
+            return "win";
+        else
+            return "draw";
+    }
+
+    else if(computerMove=="scissors"){
+        if(playerMove=="scissors")
+            return "draw";
+        else if(playerMove=="paper")
+            return "lose";
+        else
+            return "win";
+    }
+    else{
+        if(playerMove=="scissors")
+            return "win";
+        else if(playerMove=="paper")
+            return "draw";
+        else
+            return "lose";
+    }
 }
+
+function gameInit(playerMove){
+
+    computerMove = computerPlay();
+    results = playRound(playerMove, computerMove);
+    printResult(results, computerMove);
+
+}
+
+function printResult(results, computerMove){
+
+    const container = document.querySelector('#res');
+    container.textContent = 'Computer played ' + computerMove +', so you ' + results +'!';
+
+}
+
+const btns = document.querySelectorAll('button');
+
+btns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        gameInit(button.value);
+    });
+});
